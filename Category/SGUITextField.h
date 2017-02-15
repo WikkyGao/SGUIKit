15d8aeed652bc0bc86072b9571d6af9bdb847d64
Wikky <22245537@qq.com>
2017-02-15 13:45:31 +0800

    add SGUITextField class

---
+++ Category/SGUITextField.h
@@ -0,0 +1,13 @@
+//
+//  SGUITextField.h
+//  SGUIKit
+//
+//  Created by Wikky on 2017/2/10.
+//  Copyright © 2017年 Wikky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGUITextField : UITextField
+
+@end
---
+++ Category/SGUITextField.m
@@ -0,0 +1,21 @@
+//
+//  SGUITextField.m
+//  SGUIKit
+//
+//  Created by Wikky on 2017/2/10.
+//  Copyright © 2017年 Wikky. All rights reserved.
+//
+
+#import "SGUITextField.h"
+
+@implementation SGUITextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
